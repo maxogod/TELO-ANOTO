35d783cf65d66a3da442ca9a57222cda147988df
maxogod <maxi18wars@gmail.com>
2023-06-03 20:45:27 -0300

    fnhasdjk

---
+++ telo/src/utils/mockData.ts
@@ -0,0 +1,28 @@
+const hotel = {
+    id: 1,
+    name: "Hotel name",
+    location: "Hotel location",
+    picture:
+        "https://images.trvl-media.com/lodging/93000000/92550000/92541800/92541785/e51b7021.jpg",
+    stars: 3,
+    parkingLot: true,
+    availableRooms: [
+        {
+            name: "room name",
+            price: 10000,
+            id: 1,
+        },
+        {
+            name: "room name 2",
+            price: 20000,
+            id: 2,
+        },
+        {
+            name: "room name 3",
+            price: 30000,
+            id: 3,
+        },
+    ],
+};
+
+export { hotel };
