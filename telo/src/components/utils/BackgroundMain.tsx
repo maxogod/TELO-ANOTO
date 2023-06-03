913fa7523e78ec7cb5c889c69d90f4d7e17199a3
santi <santisevitz@hotmail.com>
2023-06-03 19:21:19 -0300

    poila

--- telo/src/components/utils/BackgroundMain.tsx
+++ telo/src/components/utils/BackgroundMain.tsx
@@ -1,6 +1,6 @@
 const BackgroundMain = () => {
     return (
-      <div className="fixed inset-0 bg-gradient-to-bl from-purpleGradient-0  opacity-90 via-purpleGradient-2 to-purpleGradient-3"></div>
+      <div className="  object-cover fixed inset-0 bg-gradient-to-bl from-purpleGradient-0  opacity-90 via-purpleGradient-2 to-purpleGradient-3"></div>
     );
 };
 
