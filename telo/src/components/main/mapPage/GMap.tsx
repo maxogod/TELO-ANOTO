67726d3e13e0d9f9295934f3a1d55d697055bb27
maxogod <maxi18wars@gmail.com>
2023-06-04 00:33:57 -0300

    pimpim

--- telo/src/components/main/mapPage/GMap.tsx
+++ telo/src/components/main/mapPage/GMap.tsx
@@ -4,10 +4,10 @@ import placeHolder from './placeHolder.jpg'
 
 // TODO --> HAY QUE HACER QUE SEA UN MAPA DINAMICA!!!
 const GMap = () => {
-   
+
   return (
     <div>
-     <img src={placeHolder} alt="cityPic" className='w-screen h-screen object-cover object-center fixed transform scale-150 top-40' />
+      <img src={placeHolder} alt="cityPic" className='w-screen h-screen object-cover object-center fixed transform scale-150 top-40' />
     </div>
   );
 };
