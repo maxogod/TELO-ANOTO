8b7e1135d23df678587d16852336fab5968d270d
maxogod <maxi18wars@gmail.com>
2023-06-11 01:00:22 -0300

    athus power

--- telo/src/components/hotel/HotelCard.tsx
+++ telo/src/components/hotel/HotelCard.tsx
@@ -39,6 +39,7 @@ const titleCarousel = [
     'que pinta?',
     'que sale?',
     'hoy se sale',
+    'athus',
 ]
 
 const title = titleCarousel[Math.floor(Math.random() * titleCarousel.length)]
@@ -122,7 +123,7 @@ const HotelThumbNail = ({ hotelId, roomId }: { hotelId: number, roomId: number }
     const hotel = hotels.find(h => h.id === hotelId)
     const room = hotel?.availableRooms.find(r => r.id === roomId)
 
-     const [showPopUp, setShowPopUp] = useState(false);
+    const [showPopUp, setShowPopUp] = useState(false);
 
     const handleQrCodeClick = () => {
         console.log("qr code generated");
@@ -137,23 +138,23 @@ const HotelThumbNail = ({ hotelId, roomId }: { hotelId: number, roomId: number }
 
                 <div className=" absolute mt-1 left-20 pl-1  flex flex-col text-[12px]">
                     <h1 className="text-black font-bold">{hotel?.name} - {hotel?.location}</h1>
-                    <h2  className="text-gray-700 font-bold">{room?.name} - ${room?.price}</h2 >
+                    <h2 className="text-gray-700 font-bold">{room?.name} - ${room?.price}</h2 >
                     <h3 className="text-gray-700 text-[10px]">FALTA ROOM DATE</h3>
                     <div className="flex  mt-[1px]  space-x-4 ">
-                    <img src={mapThumbNail}  className='w-5 h-5 ' alt="" />
-                    <img src={cancel} className='w-5 h-5 ' alt="" />
-                </div>
+                        <img src={mapThumbNail} className='w-5 h-5 ' alt="" />
+                        <img src={cancel} className='w-5 h-5 ' alt="" />
+                    </div>
                 </div>
 
 
-                <div className='absolute bg-reservationPurple w-20 h-20 right-0 rounded-3xl  flex justify-center items-center'  onClick={handleQrCodeClick}>
+                <div className='absolute bg-reservationPurple w-20 h-20 right-0 rounded-3xl  flex justify-center items-center' onClick={handleQrCodeClick}>
                     <img src={qrCode} className='w-14 h-14' alt="" />
                 </div>
                 <div className='absolute bg-black w-20 h-20 left-0 rounded-3xl flex justify-center items-center'>
-                      <img src={hotel?.picture} className='w-full h-full rounded-3xl' alt="" />
+                    <img src={hotel?.picture} className='w-full h-full rounded-3xl' alt="" />
                 </div>
             </div>
-            {showPopUp && <QrPopUp setShowPopUp={setShowPopUp} hotel={hotel as Hotel} room={room as Room}  />}
+            {showPopUp && <QrPopUp setShowPopUp={setShowPopUp} hotel={hotel as Hotel} room={room as Room} />}
         </div>
     )
 }
